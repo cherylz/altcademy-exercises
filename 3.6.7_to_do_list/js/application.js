6e7a83baaebbb297372fe613f545e45a7e8b78de
cz <deutschessz@gmail.com>
2018-12-21 15:32:06 +0800

    code abstraction, removed unnecessary api requests, added a random quote generator, removed notes to myself in comments,and made formatting consistent.

--- 3.6.7_to_do_list/js/application.js
+++ 3.6.7_to_do_list/js/application.js
@@ -1,31 +1,51 @@
 var httpRequest = new XMLHttpRequest();
 
+var getRandomQuote = function() {
+  $.ajax({
+    type: 'GET',
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous&count=1',
+    dataType: 'json',
+    success: function(response) {
+      document.querySelector('.random-quote').innerHTML = '"' + response[0].quote + '" - ' + response[0].author;
+    },
+    error: function() {
+      document.querySelector('.random-quote').innerHTML = 'Always get plenty of water.';
+    },
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader('X-Mashape-Authorization',  'puULGOKKBImshKkwZXLFCZDJYCdWp1bUI0hjsnWtzMDWBArpZq');
+    }
+  });
+};
+
+// Update the todo item (only if edited) in the server.
 var updateTodoEdited = function (targetNode) {
-  httpRequest.onload = function() {
+  httpRequest.onload = function () {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 200) {
-        console.log('The todo content is modified.', httpRequest.responseText); // Question: How to prevent duplicate PUT requests to the server and duplicate work in the broswer when the event is updated via Enter key and then loses focus (i.e. blurs)?
+        console.log('The todo content is modified.', httpRequest.responseText);
       } else {
         console.log(httpRequest.statusText);
       }
     }
   }
-  httpRequest.onerror = function() {
+  httpRequest.onerror = function () {
     console.log(httpRequest.statusText);
   }
-  var input = targetNode.value; // Question: Is it needed and if yes, how to set a if statement so when the input remains the same, the value won't be updated?
-  targetNode.setAttribute('value', input);
-  var id = targetNode.parentElement.parentElement.getAttribute("data-todoid");
-  httpRequest.open('PUT', 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '?api_key=52');
-  httpRequest.setRequestHeader("Content-Type", "application/json");
-  httpRequest.send(JSON.stringify({
-    task: {
-      content: input
-    }
-  }));
+  var input = targetNode.value;
+  if (input !== targetNode.getAttribute('value')) {
+    targetNode.setAttribute('value', input);
+    var id = targetNode.parentElement.parentElement.getAttribute('data-todoid');
+    httpRequest.open('PUT', 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '?api_key=52');
+    httpRequest.setRequestHeader('Content-Type', 'application/json');
+    httpRequest.send(JSON.stringify({
+      task: {
+        content: input
+      }
+    }));
+  }
 };
 
-// Update an edited todo item when the item is not focused
+// Update an edited todo item when it lost focus.
 var editTodoWhenBlur = function () {
   var editTodo = document.querySelectorAll('.edit-content');
   for (var i = 0; i < editTodo.length; i++) {
@@ -34,6 +54,11 @@ var editTodoWhenBlur = function () {
     });
   }
 };
+
+var styleCompletedTodo = function(item) {
+  item.querySelector('span').setAttribute('class', 'custom-checkbox');
+  item.querySelector('.edit-content').style.textDecoration = 'line-through';
+}
 
 var renderTodoItem = function (id, status, content) {
   var todoGroup = document.querySelector('#todo-group');
@@ -48,52 +73,64 @@ var renderTodoItem = function (id, status, content) {
   // Style the completed todo items.
   if (status) {
     newTodo.querySelector('input[type="checkbox"]').checked = true;
-    newTodo.querySelector('span').setAttribute('class', 'custom-checkbox');
-    newTodo.querySelector('.edit-content').style.textDecoration = 'line-through';
+    styleCompletedTodo(newTodo);
   }
 };
 
-var renderTodoList = function () {
-  httpRequest.onload = function() {
+var renderTodoList = function (status) {
+  httpRequest.onload = function () {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 200) {
-        console.log('Todo list retrieved from the server.', httpRequest.responseText);
+        console.log('Todo list (raw and full) retrieved from the server.', httpRequest.responseText);
         var todoGroup = document.querySelector('#todo-group');
         while (todoGroup.firstChild) {
           todoGroup.removeChild(todoGroup.firstChild);
         }
-        var todoListFromServer = JSON.parse(httpRequest.responseText)["tasks"];
+        var todoListFromServer = JSON.parse(httpRequest.responseText)['tasks'];
         // Sort the todo items by id.
         todoListFromServer.sort(function (a, b) {
           return a.id - b.id;
         });
-        todoListFromServer.forEach(function (item) {
-          renderTodoItem(item.id, item.completed, item.content);
-        });
+        // Render the todo list based on status.
+        if (status === 'active') {
+         todoListFromServer.forEach(function (item) {
+           if (!item.completed) {
+             renderTodoItem(item.id, item.completed, item.content);
+           }
+         });
+       } else if (status === 'completed') {
+          todoListFromServer.forEach(function (item) {
+            if (item.completed) {
+              renderTodoItem(item.id, item.completed, item.content);
+            }
+          });
+        } else if (status === 'all') {
+          todoListFromServer.forEach(function (item) {
+            renderTodoItem(item.id, item.completed, item.content);
+          });
+        }
       } else {
         console.log(httpRequest.statusText);
       }
     }
   }
-  httpRequest.onerror = function() {
+  httpRequest.onerror = function () {
     console.log(httpRequest.statusText);
   }
   httpRequest.open('GET', 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=52');
   httpRequest.send();
 };
 
-// Add a new task.
 var addTodo = function () {
   var todoGroup = document.querySelector('#todo-group');
   var addSection = document.querySelector('#add-todo');
-  var input = addSection.querySelector('input').value;
-  // TBC: add fallback for input that contains elements like <textarea> and <input>.
+  var input = addSection.querySelector('input').value; // Study notes: No need to add fallback for input value that contains characters with special meaning such as "&" and HTML elements like <textarea> and <input> because the string in the request is encoded. For example, <input> is encoded to \u003cinput\u003e.
   httpRequest.onload = function () {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 200) {
         console.log('This new todo is stored in the server.', httpRequest.responseText);
         // To render this new todo item & to store extra info (id, completion status) about it.
-        var todoItemFromServer = JSON.parse(httpRequest.responseText)["task"];
+        var todoItemFromServer = JSON.parse(httpRequest.responseText)['task'];
         renderTodoItem(todoItemFromServer.id, todoItemFromServer.completed, todoItemFromServer.content);
         addSection.querySelector('input').value = '';
       } else {
@@ -106,7 +143,7 @@ var addTodo = function () {
   }
   if (input) {
     httpRequest.open('POST', 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=52');
-    httpRequest.setRequestHeader("Content-Type", "application/json");
+    httpRequest.setRequestHeader('Content-Type', 'application/json');
     httpRequest.send(JSON.stringify({
       task: {
         content: input
@@ -116,31 +153,30 @@ var addTodo = function () {
 };
 
 var markComplete = function (targetNode) {
-  httpRequest.onload = function() {
+  httpRequest.onload = function () {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 200) {
         targetNode.parentElement.setAttribute('data-completed', 'true');
-        targetNode.parentElement.querySelector('span').setAttribute('class', 'custom-checkbox');
-        targetNode.parentElement.querySelector('.edit-content').style.textDecoration = 'line-through';
-        console.log('This todo is marked as completed', httpRequest.responseText);
+        styleCompletedTodo(targetNode.parentElement);
+        console.log('This todo is marked as completed.', httpRequest.responseText);
       } else {
         console.log(httpRequest.statusText);
       }
     }
   }
-  httpRequest.onerror = function() {
+  httpRequest.onerror = function () {
     console.log(httpRequest.statusText);
   }
-  var id = targetNode.parentElement.getAttribute("data-todoid");
+  var id = targetNode.parentElement.getAttribute('data-todoid');
   httpRequest.open('PUT', 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '/mark_complete?api_key=52');
   httpRequest.send();
 };
 
 var markActive = function (targetNode) {
-  httpRequest.onload = function() {
+  httpRequest.onload = function () {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 200) {
-        console.log('This task is marked as active', httpRequest.responseText);
+        console.log('This task is marked as active.', httpRequest.responseText);
         targetNode.parentElement.setAttribute('data-completed', 'false');
         targetNode.parentElement.querySelector('span').removeAttribute('class');
         targetNode.parentElement.querySelector('.edit-content').style.textDecoration = '';
@@ -149,16 +185,16 @@ var markActive = function (targetNode) {
       }
     }
   }
-  httpRequest.onerror = function() {
+  httpRequest.onerror = function () {
     console.log(httpRequest.statusText);
   }
-  var id = targetNode.parentElement.getAttribute("data-todoid");
+  var id = targetNode.parentElement.getAttribute('data-todoid');
   httpRequest.open('PUT', 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '/mark_active?api_key=52');
   httpRequest.send();
 };
 
 var deleteTodo = function (targetNode) {
-  httpRequest.onload = function() {
+  httpRequest.onload = function () {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 200) {
         console.log('The task is deleted.', httpRequest.responseText);
@@ -168,118 +204,57 @@ var deleteTodo = function (targetNode) {
       }
     }
   }
-  httpRequest.onerror = function() {
+  httpRequest.onerror = function () {
     console.log(httpRequest.statusText);
   }
-  var id = targetNode.parentElement.parentElement.getAttribute("data-todoid");
+  var id = targetNode.parentElement.parentElement.getAttribute('data-todoid');
   httpRequest.open('DELETE', 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '?api_key=52');
   httpRequest.send();
 }
 
-var showActive = function () {
-  httpRequest.onload = function() {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        console.log('Active todos.', httpRequest.responseText);
-        var todoGroup = document.querySelector('#todo-group');
-        while (todoGroup.firstChild) {
-          todoGroup.removeChild(todoGroup.firstChild);
-        }
-        var todoListFromServer = JSON.parse(httpRequest.responseText)["tasks"];
-        todoListFromServer.sort(function (a, b) {
-          return a.id - b.id;
-        });
-        todoListFromServer.forEach(function (item) {
-          if (!item.completed) {
-            renderTodoItem(item.id, item.completed, item.content);
-          }
-        });
-      } else {
-        console.log(httpRequest.statusText);
-      }
-    }
-  }
-  httpRequest.onerror = function() {
-    console.log(httpRequest.statusText);
-  }
-  httpRequest.open('GET', 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=52');
-  httpRequest.send();
-};
-
-var showCompleted = function () {
-  httpRequest.onload = function() {
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        console.log('Completed todos.', httpRequest.responseText);
-        var todoGroup = document.querySelector('#todo-group');
-        while (todoGroup.firstChild) {
-          todoGroup.removeChild(todoGroup.firstChild);
-        }
-        var todoListFromServer = JSON.parse(httpRequest.responseText)["tasks"];
-        todoListFromServer.sort(function (a, b) {
-          return a.id - b.id;
-        });
-        todoListFromServer.forEach(function (item) {
-          if (item.completed) {
-            renderTodoItem(item.id, item.completed, item.content);
-          }
-        });
-      } else {
-        console.log(httpRequest.statusText);
-      }
-    }
-  }
-  httpRequest.onerror = function() {
-    console.log(httpRequest.statusText);
-  }
-  httpRequest.open('GET', 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=52');
-  httpRequest.send();
-}
-
-
-document.addEventListener('DOMContentLoaded', function() {
-
-  renderTodoList();
-
+document.addEventListener('DOMContentLoaded', function () {
   var filter = document.querySelector('#filter');
   var todoGroup = document.querySelector('#todo-group');
   var addSection = document.querySelector('#add-todo');
 
+  getRandomQuote();
+
+  renderTodoList('all');
+
+  // Filter tasks based on the status.
   filter.querySelector('.all-btn').addEventListener('click', function () {
-    renderTodoList();
+    renderTodoList('all');
   });
-
   filter.querySelector('.active-btn').addEventListener('click', function () {
-    showActive();
+    renderTodoList('active');
   });
-
   filter.querySelector('.completed-btn').addEventListener('click', function () {
-    showCompleted();
+    renderTodoList('completed');
   });
 
-  // Update an edited todo item when the Enter key is pressed on that item
+  // Update an edited todo item when the Enter key is pressed on it.
   todoGroup.addEventListener('keyup', function (event) {
-    if (event.target.className == 'edit-content' && event.key == 'Enter') {
+    if (event.target.className === 'edit-content' && event.key === 'Enter') {
       updateTodoEdited(event.target);
     }
   });
 
-  // Add a todo item via two ways
+  // Add a todo item via two ways.
   addSection.addEventListener('click', function (event) {
-    if (event.target.className == 'fas fa-plus-circle') {
+    if (event.target.parentElement.matches('.add')) {
       addTodo();
     }
   });
   addSection.addEventListener('keyup', function (event) {
-    if (event.key == 'Enter') {
+    if (event.key === 'Enter') {
       addTodo();
     }
   });
 
-  // Mark complete or active
+  // Mark complete or active.
   todoGroup.addEventListener('click', function (event) {
-    if (event.target.type == 'checkbox') {
-      if (event.target.checked == true) {
+    if (event.target.type === 'checkbox') {
+      if (event.target.checked === true) {
         markComplete(event.target);
       } else {
         markActive(event.target);
@@ -287,19 +262,10 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-  // Remove a todo item
+  // Remove a todo item.
   todoGroup.addEventListener('click', function (event) {
     if (event.target.parentElement.matches('.remove')) {
       deleteTodo(event.target);
     }
   });
 });
-
-/** Important Question: Why is it called more than one time every time the target is blurred and why does the number of calls keep adding up by one?
-todoGroup.addEventListener('blur', function (event) {
-  console.log(event.target.className);
-  if (event.target.className == 'edit-content') {
-      console.log('trees');
-  }
-});
-*/
